efc1f6225026cc60c0b501ac12ecfa34000456cb
Ayesha116 <ayeshajawed1168@gmail.com>
2021-01-19 23:31:34 +0500

    data fetched from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import {fetchquestion} from './services/Apiservice'
 import './App.css';
 
 function App() {
+
+  useEffect(()=>{
+    const datafromapi = async()=> {
+      await fetchquestion(10,'easy')
+      
+    }
+    datafromapi()
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hey
     </div>
   );
 }
